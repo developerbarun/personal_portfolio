d57a9b6d9e4a07f367ff186b660377eab2722167
developerbarun <bkggamer13@gmail.com>
2024-05-11 14:47:28 +0530

    Library changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tilt } from "/node_modules/.vite/deps/react-tilt.js";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
