edd1ebd2ad66993f060664a8a16c1aafa4fc9159
developerbarun <bkggamer13@gmail.com>
2024-05-13 18:49:53 +0530

    CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = ()=>{
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="sm:bg-black bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
         </div>
