ccbcab064683d6477c04331a56dc642fcf165ee0
developerbarun <bkggamer13@gmail.com>
2024-05-13 18:45:01 +0530

    Mobile Devices changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  if (isMobile) return null;
+  // if (isMobile) return null;
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
